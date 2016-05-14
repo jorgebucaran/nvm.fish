d382f88258382023c1d79b17945a78abf469d62c
Jorge Bucaran <j@bucaran.me>
2016-05-14 20:20:38 +0900

    Add special completion to hide dir and files when no versions are installed.

--- completions/fin.fish
+++ completions/fin.fish
@@ -1,4 +1,3 @@
-complete -xc fin -d "node.js version manager" -a "\t"
 complete -xc fin -s h -l help -d "Show usage help"
 complete -xc fin -s v -l version -d "Show version information"
 complete -xc fin -n "__fish_use_subcommand" -a use -d "Select version"
@@ -26,4 +25,6 @@ if set -l versions (__fin_version_local)
 
         complete -xc fin -a "$ver" -d "$info"
     end
+else
+    complete -xc fin -a "\t"
 end
