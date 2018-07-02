4805b5d53717d16aa99a5139eb8b17421e015e1b
Dev Aggarwal <devxpy@gmail.com>
2018-07-02 21:07:27 +0530

    Update fnm.fish

--- conf.d/fnm.fish
+++ conf.d/fnm.fish
@@ -22,3 +22,25 @@ if test -d "$fnm_config/bin"
         set PATH "$fnm_config/bin" $PATH
     end
 end
+
+# Adds locally installed NodeJS npm binary executable modules to the path
+# (stolen from https://github.com/oh-my-fish/plugin-node) 
+
+if not type -q npm
+  if test -d /usr/local/share/npm/bin
+    if not contains /usr/local/share/npm/bin $PATH
+      set PATH /usr/local/share/npm/bin $PATH
+    end
+  else if test -n "$NPM_DIR"; and test -d $NPM_DIR
+    if not contains $NPM_DIR $PATH
+      set PATH $NPM_DIR $PATH
+    end
+  else
+    echo "plugin-node: npm is unavailable, either install it or set $NPM_DIR"
+    echo "             in the config.fish file: set NPM_DIR /path/to/npm/dir"
+  end
+end
+
+if not contains ./node_modules/.bin $PATH
+  set PATH ./node_modules/.bin $PATH
+end
