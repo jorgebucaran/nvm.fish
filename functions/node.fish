684655f98072dc56b1b5ae533aedc4c61d70b5d1
WEBER Logan <logan.weber@airbus.com>
2018-12-21 11:31:25 +0100

    feat(node): use local node binary if existing

--- functions/node.fish
+++ functions/node.fish
@@ -1,3 +1,7 @@
 function node -d "Server-side JavaScript runtime" -w node
-    __fnm_run_bin_as "node" $argv
+    if test -e "node_modules/.bin/node"
+        node_modules/.bin/node $argv
+    else
+        __fnm_run_bin_as "node" $argv
+    end
 end
