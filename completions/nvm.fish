7d080c0c8cafd9445217d5aa104a17b790060edc
Jorge Bucaran <mail@jorgebucaran.com>
2020-11-28 03:04:32 +0900

    Add uninstall completions

--- completions/nvm.fish
+++ completions/nvm.fish
@@ -5,11 +5,13 @@ complete -c nvm --exclusive --condition "__fish_use_subcommand" -a install -d ""
 complete -c nvm --exclusive --condition "__fish_use_subcommand" -a use -d ""
 complete -c nvm --exclusive --condition "__fish_use_subcommand" -a list -d ""
 complete -c nvm --exclusive --condition "__fish_use_subcommand" -a list-remote -d ""
-complete -c nvm --exclusive --condition "__fish_use_subcommand" -a remove -d ""
+complete -c nvm --exclusive --condition "__fish_use_subcommand" -a uninstall -d ""
 complete -c nvm --exclusive --condition "__fish_use_subcommand" -a current -d ""
 complete -c nvm --exclusive --condition "__fish_seen_subcommand_from install" -a "(string split ' ' <$nvm_data/.index)"
-complete -c nvm --exclusive --condition "__fish_seen_subcommand_from remove" -a "(_nvm_list | string split ' ' | string replace system '')"
 complete -c nvm --exclusive --condition "__fish_seen_subcommand_from use" -a "(_nvm_list | string split ' ')"
-
-set -q nvm_default_version \
-    && complete -c nvm --exclusive --condition "__fish_seen_subcommand_from use remove" -a default
+complete -c nvm --exclusive --condition "__fish_seen_subcommand_from uninstall" -a "(
+    _nvm_list | string split ' ' | string replace system ''
+)"
+complete -c nvm --exclusive --condition "__fish_seen_subcommand_from use uninstall" -a "(
+    set --query nvm_default_version && echo default
+)"
