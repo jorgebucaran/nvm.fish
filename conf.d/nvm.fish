eb775a74f6a5b19026a653f3425a90696b73d36b
Jorge Bucaran <mail@jorgebucaran.com>
2020-12-04 15:53:40 +0900

    Send install event errors to null

--- conf.d/nvm.fish
+++ conf.d/nvm.fish
@@ -1,4 +1,5 @@
 set --global nvm_version 1.1.0
+
 set --query XDG_DATA_HOME \
     && set --global nvm_data $XDG_DATA_HOME/nvm \
     || set --global nvm_data ~/.local/share/nvm
@@ -10,19 +11,15 @@ end
 
 function _nvm_install -e nvm_install
     test ! -d $nvm_data && command mkdir -p $nvm_data
-    echo "Updating the Node download index for the first time..."
+    echo "Downloading the Node distribution index for the first time..." 2>/dev/null
     _nvm_index_update $nvm_mirror/index.tab $nvm_data/.index
 end
 
 function _nvm_uninstall -e nvm_uninstall
     command rm -rf $nvm_data
-    
     set --query nvm_current_version && _nvm_version_deactivate $nvm_current_version
 
-    for var in nvm_{version,current_version,default_node,data}
-        set --erase $var
-    end
-
-    complete --erase --command nvm
+    set --names | string replace --filter --regex "^nvm_" -- "set --erase nvm_" | source
     functions --erase (functions --all | string match --entire --regex "^_nvm_")
+    complete --erase --command nvm
 end
