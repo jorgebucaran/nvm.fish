476a9e63d9bfb07971469462c62cbd77fb6997d4
Jorge Bucaran <mail@jorgebucaran.com>
2021-01-16 19:34:06 +0900

    Suppress stderr

--- tests/nvm.fish
+++ tests/nvm.fish
@@ -1,14 +1,14 @@
 @test "nvm install latest major.minor.patch" (
-  nvm install latest >/dev/null
+  nvm install latest >/dev/null 2>&1
   nvm current
 ) = (nvm list-remote | string match --regex -- "v\d+\.\d+\.\d+")[-1]
 
 @test "nvm install latest minor.patch" (
-  nvm install 5 >/dev/null
+  nvm install 5 >/dev/null 2>&1
   nvm current
 ) = v5.12.0
 
 @test "nvm install latest patch" (
-  nvm install v5.11 >/dev/null
+  nvm install v5.11 >/dev/null 2>&1
   nvm current
 ) = v5.11.1
