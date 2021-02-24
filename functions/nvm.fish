02d4a8214c32bbd5ef18e94b776284dd9770c16c
LeeroyDing <1302683+LeeroyDing@users.noreply.github.com>
2021-02-24 12:16:19 +0000

    Try installing node from source

--- functions/nvm.fish
+++ functions/nvm.fish
@@ -79,20 +79,33 @@ function nvm --argument-names cmd v --description "Node version manager"
                 echo -e "Fetching \x1b[4m$url\x1b[24m\x1b[7m"
 
                 if ! command curl --progress-bar --location $url \
-                        | command tar --extract --gzip --directory $nvm_data/$v 2>/dev/null
-                    command rm -rf $nvm_data/$v
+                        | command tar --extract --gzip --strip-components=1 --directory $nvm_data/$v 2>/dev/null
                     echo -e "\033[F\33[2K\x1b[0mnvm: Invalid mirror or host unavailable: \"$url\"" >&2
-                    return 1
+                    # return 1
+                    
+                    echo -e "Trying to install from source..."
+
+                    set --local dir "node-$v"
+                    set --local url $nvm_mirror/$v/$dir.$ext
+                    command mkdir -p $nvm_data/src/$v
+                    command curl --progress-bar --location $url \
+                        | command tar --extract --gzip --strip-components=1 --directory $nvm_data/src/$v 2>/dev/null
+                    pushd $nvm_data/src/$v
+                    command /bin/sh $nvm_data/src/$v/configure --prefix="$nvm_data/$v"
+                    command make -j9
+                    command make install
+                    popd
+                    command rm -rf $nvm_data/src/$v
                 end
 
                 echo -en "\033[F\33[2K\x1b[0m"
 
-                if test "$os" = win
-                    command mv $nvm_data/$v/$dir $nvm_data/$v/bin
-                else
-                    command mv $nvm_data/$v/$dir/* $nvm_data/$v
-                    command rm -rf $nvm_data/$v/$dir
-                end
+                # if test "$os" = win
+                #     command mv $nvm_data/$v/$dir $nvm_data/$v/bin
+                # else
+                #     command mv $nvm_data/$v/$dir/* $nvm_data/$v
+                #     command rm -rf $nvm_data/$v/$dir
+                # end
             end
 
             if test $v != "$nvm_current_version"
