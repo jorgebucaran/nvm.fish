e9471233ac195ff22c485bde71213e69a8239387
Jorge Bucaran <mail@jorgebucaran.com>
2021-02-27 01:05:57 +0900

    Upgrade variable state during update
    We can eventually get rid of this, perhaps
    after several minors or in the next major.
    
    Close #141

--- conf.d/nvm.fish
+++ conf.d/nvm.fish
@@ -1,11 +1,17 @@
 function _nvm_install --on-event nvm_install
-    set --query nvm_mirror || set --universal nvm_mirror https://nodejs.org/dist
     set --query XDG_DATA_HOME || set --local XDG_DATA_HOME ~/.local/share
     set --universal nvm_data $XDG_DATA_HOME/nvm
+    set --query nvm_mirror || set --universal nvm_mirror https://nodejs.org/dist
 
     test ! -d $nvm_data && command mkdir -p $nvm_data
     echo "Downloading the Node distribution index for the first time..." 2>/dev/null
     _nvm_index_update $nvm_mirror $nvm_data/.index
+end
+
+function _nvm_update --on-event nvm_update
+    set --query XDG_DATA_HOME || set --local XDG_DATA_HOME ~/.local/share
+    set --universal nvm_data $XDG_DATA_HOME/nvm
+    set --query nvm_mirror || set --universal nvm_mirror https://nodejs.org/dist
 end
 
 function _nvm_uninstall --on-event nvm_uninstall
