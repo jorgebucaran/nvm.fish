eb2268b7d5dbaea77fcbf6d2ad1ce8903d3a537f
Alexander Liu <liujalexander84@gmail.com>
2021-03-13 12:41:00 -0800

    refactor to ditch eval for node info

--- functions/nvm.fish
+++ functions/nvm.fish
@@ -188,10 +188,7 @@ function _nvm_current
 end
 
 function _nvm_node_info
-    set --local npm_path (string replace bin/npm-cli.js "" (realpath (command --search npm)))
-    command node --eval "
-        console.log(process.version)
-        console.log(require('"$npm_path"/package.json').version)
-        console.log(process.execPath.replace(os.homedir(), '~'))
-    "
+    command node -v
+    command npm -v
+    string replace $HOME "~" (realpath (command --search node))
 end
