37cd7cffe39e240deac459ca703298b720570d05
Jorge Bucaran <mail@jorgebucaran.com>
2021-09-18 15:17:29 +0900

    Download index before processing it, fix #154

--- functions/_nvm_index_update.fish
+++ functions/_nvm_index_update.fish
@@ -1,12 +1,16 @@
 function _nvm_index_update --argument-names mirror index
-    command curl --location --silent $mirror/index.tab | command awk -v OFS=\t '
+    if not command curl --location --silent $mirror/index.tab >$index.temp
+        command rm -f $index.temp
+        echo "nvm: Can't update index, host unvailable: \"$mirror\"" >&2
+        return 1
+    end
+
+    command awk -v OFS=\t '
         /v0.9.12/ { exit } # Unsupported
         NR > 1 {
             print $1 (NR == 2  ? " latest" : $10 != "-" ? " lts/" tolower($10) : "")
         }
-    ' >$index.temp 2>/dev/null && command mv $index.temp $index && return
+    ' $index.temp >$index
 
     command rm -f $index.temp
-    echo "nvm: Invalid index or unavailable host: \"$mirror\"" >&2
-    return 1
 end
