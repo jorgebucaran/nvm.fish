5b63e0538a6518a49d3b59aa34909db30930d28e
Jorge Bucaran <mail@jorgebucaran.com>
2022-05-31 18:07:07 -0300

    2.2.9

--- functions/nvm.fish
+++ functions/nvm.fish
@@ -29,7 +29,7 @@ function nvm --description "Node version manager"
 
     switch "$cmd"
         case -v --version
-            echo "nvm, version 2.2.8"
+            echo "nvm, version 2.2.9"
         case "" -h --help
             echo "Usage: nvm install <version>    Download and activate the specified Node version"
             echo "       nvm install              Install version from nearest .nvmrc file"
