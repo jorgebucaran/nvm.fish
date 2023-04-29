d2939b612cf00a9221da6fb5becd5a536237f1e5
Jorge Bucaran <mail@jorgebucaran.com>
2023-04-29 22:37:03 +0900

    Change $nvm_data scope back to global
    Fix #190

--- conf.d/nvm.fish
+++ conf.d/nvm.fish
@@ -1,7 +1,8 @@
+set --query XDG_DATA_HOME || set --local XDG_DATA_HOME ~/.local/share
+set --global nvm_data $XDG_DATA_HOME/nvm
+
 function _nvm_install --on-event nvm_install
     set --query nvm_mirror || set --universal nvm_mirror https://nodejs.org/dist
-    set --query XDG_DATA_HOME || set --local XDG_DATA_HOME ~/.local/share
-    set --universal nvm_data $XDG_DATA_HOME/nvm
 
     test ! -d $nvm_data && command mkdir -p $nvm_data
     echo "Downloading the Node distribution index..." 2>/dev/null
@@ -10,8 +11,6 @@ end
 
 function _nvm_update --on-event nvm_update
     set --query nvm_mirror || set --universal nvm_mirror https://nodejs.org/dist
-    set --query XDG_DATA_HOME || set --local XDG_DATA_HOME ~/.local/share
-    set --universal nvm_data $XDG_DATA_HOME/nvm
 end
 
 function _nvm_uninstall --on-event nvm_uninstall
