aa5ccbde94b8424d29670ca26638ade03e84cdcd
Jorge Bucaran <mail@jorgebucaran.com>
2023-04-29 23:00:53 +0900

    Migrate scope change via update handler

--- conf.d/nvm.fish
+++ conf.d/nvm.fish
@@ -1,4 +1,4 @@
-set --query nvm_mirror || set --universal nvm_mirror https://nodejs.org/dist
+set --query nvm_mirror || set --global nvm_mirror https://nodejs.org/dist
 set --query XDG_DATA_HOME || set --local XDG_DATA_HOME ~/.local/share
 set --global nvm_data $XDG_DATA_HOME/nvm
 
@@ -6,6 +6,12 @@ function _nvm_install --on-event nvm_install
     test ! -d $nvm_data && command mkdir -p $nvm_data
     echo "Downloading the Node distribution index..." 2>/dev/null
     _nvm_index_update
+end
+
+function _nvm_update --on-event nvm_update
+    set --query --universal nvm_data && set --erase --universal nvm_data
+    set --query --universal nvm_mirror && set --erase --universal nvm_mirror
+    set --query nvm_mirror || set --global nvm_mirror https://nodejs.org/dist
 end
 
 function _nvm_uninstall --on-event nvm_uninstall
