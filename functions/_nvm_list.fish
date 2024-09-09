8d36c8df6a150e3cf9c9508c2a10b01bf60768ee
nv4re <tosbodesz@gmail.com>
2024-09-09 10:51:46 +0700

    update _nvm_list to correctly match version directory names by subtracting path

--- functions/_nvm_list.fish
+++ functions/_nvm_list.fish
@@ -1,8 +1,9 @@
 function _nvm_list
     set --local versions $nvm_data/*
+    set --local installed_versions (string replace -a -- $nvm_data/ "" $versions)
+
     set --query versions[1] &&
-        string match --entire --regex -- (string match --regex -- "/v\d.+" $versions |
-            string replace --regex -- "^/" "" |
+        string match --entire --regex -- (string match --regex -- "v\d.+" $installed_versions |
             string escape --style=regex |
             string join "|"
         ) <$nvm_data/.index
